61ad051b7be5e28e0ecb67a05410c3b009844dec
M. Abyan Rizqo <rizqo1412@gmail.com>
2023-11-25 17:52:33 +0700

    feat: add foreign key in outlet menu table

--- db/migrations/000003_outlet_menu_table.up.sql
+++ db/migrations/000003_outlet_menu_table.up.sql
@@ -1,29 +1,27 @@
+-- Create the 'outlets_menus' table with columns 'outlet_id', 'menu_id', 'is_available', 'created_at', and 'updated_at'
 CREATE TABLE outlets_menus (
-  "outlet_id" integer NOT NULL ,
-  "menu_id" integer NOT NULL ,
-  "is_available" bool NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now()),
-  "updated_at" timestamp NOT NULL DEFAULT (now()),
+  outlet_id integer NOT NULL,
+  menu_id integer NOT NULL,
+  is_available bool NOT NULL,
+  created_at timestamp NOT NULL DEFAULT (now()),
+  updated_at timestamp NOT NULL DEFAULT (now()),
   CONSTRAINT outlet_menu_unq UNIQUE (outlet_id, menu_id)
 );
 
-CREATE INDEX outlet_menu_outlet_id ON outlets_menus(outlet_id);
-CREATE INDEX outlet_menu_omenu_id ON outlets_menus(menu_id);
+-- Add foreign keys to reference 'menu_id' and 'outlet_id' columns to 'id' columns in 'menus' and 'outlets' tables respectively
+ALTER TABLE outlets_menus ADD FOREIGN KEY (menu_id) REFERENCES menus (id);
+ALTER TABLE outlets_menus ADD FOREIGN KEY (outlet_id) REFERENCES outlets (id);
 
-DO $$ 
+-- Create indexes on 'outlets_menus' table
+CREATE UNIQUE INDEX ON outlets_menus (outlet_id, menu_id);
+CREATE INDEX outlet_menu_menu_id ON outlets_menus(menu_id);
+
+-- Insert data into 'outlets_menus' table using a loop
+DO $$
 BEGIN
   FOR i IN 1..30 LOOP
     INSERT INTO outlets_menus (menu_id, outlet_id, is_available)
-    VALUES 
-    (i, 1, random()>0.15),
-    (i, 2, random()>0.15),
-    (i, 3, random()>0.15),
-    (i, 4, random()>0.15),
-    (i, 5, random()>0.15),
-    (i, 6, random()>0.15),
-    (i, 7, random()>0.15),
-    (i, 8, random()>0.15),
-    (i, 9, random()>0.15),
-    (i, 10, random()>0.15);
+    SELECT i, j, random() > 0.15
+    FROM generate_series(1, 10) AS j;
   END LOOP;
 END $$;
