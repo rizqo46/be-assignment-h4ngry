bbea5671ebe2bd483a79a2d86bb0a1f0e93b8426
M. Abyan Rizqo <rizqo1412@gmail.com>
2023-11-26 11:15:39 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import metadata from './metadata';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.PORT, 10) || 3000
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setVersion('1.0')
+    .build();
+    
+  await SwaggerModule.loadPluginMetadata(metadata);
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
