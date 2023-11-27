a60675e957d0a6bf45f159fe64fd01f4fce73a01
M. Abyan Rizqo <rizqo1412@gmail.com>
2023-11-27 09:49:28 +0700

    feat: add auth berar api for swagger on orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -11,7 +11,9 @@ import { OrdersService } from './orders.service';
 import { JWTGuard } from 'src/auth/auth.guard';
 import { Request as RequestExpress } from 'express';
 import { OrderReqDto } from './dto/orders.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
